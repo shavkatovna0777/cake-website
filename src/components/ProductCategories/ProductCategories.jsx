bdcf343b21b8c8721ed5f35b96076de1fd3b5bf3
Kamola Soxibnazarova <135108613+shavkatovna0777@users.noreply.github.com>
2025-03-14 17:43:57 +0500

    #

--- src/components/ProductCategories/ProductCategories.jsx
+++ src/components/ProductCategories/ProductCategories.jsx
@@ -2,32 +2,33 @@ import { HiOutlineArrowSmallRight } from "react-icons/hi2";
 import { Button } from "../../ui/Button.jsx";
 import Img from "../LazyLoadImg/Img.jsx";
 
-
-export default function ProductCategories({categoriesProduct}) {
+export default function ProductCategories({ categoriesProduct }) {
   return (
-    <div className="container mx-auto  rounded-lg border">
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-[36px] font-semibold ">Kategoriyalar</h2>
-        <Button variant="ghost" className="flex items-center gap-[5px]">
-          Barchasini ko‘rish
-          <HiOutlineArrowSmallRight />
-        </Button>
+    <section>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-[36px] font-semibold ">Kategoriyalar</h2>
+          <Button variant="ghost" className="flex items-center gap-[5px]">
+            Barchasini ko‘rish
+            <HiOutlineArrowSmallRight />
+          </Button>
+        </div>
+        <div className="grid grid-cols-5 gap-4">
+          {categoriesProduct.map((category, index) => (
+            <div
+              key={index}
+              className="text-center border rounded-lg p-2 shadow-sm"
+            >
+              <Img
+                src={category.image}
+                alt={category.name}
+                className="w-full h-32 object-cover rounded-md"
+              />
+              <p className="mt-2 text-sm font-semibold">{category.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-5 gap-4">
-        {categoriesProduct.map((category, index) => (
-          <div
-            key={index}
-            className="text-center border rounded-lg p-2 shadow-sm"
-          >
-            <Img
-              src={category.image}
-              alt={category.name}
-              className="w-full h-32 object-cover rounded-md"
-            />
-            <p className="mt-2 text-sm font-semibold">{category.name}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    </section>
   );
 }
